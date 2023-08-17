ea84093c337ff32aa3c317dd8dbcd50dbfd29358
abdallah boufrad <abdallahboufrad@gmail.com>
2023-08-17 17:03:36 +0100

    Rails Watch List

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+  resources :bookmarks
+  end
+  resources :bookmarks
 end
